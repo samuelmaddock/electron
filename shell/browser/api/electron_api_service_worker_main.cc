3d50328014b4f0245a5e198d83210de2218170e1
Samuel Maddock <samuel.maddock@gmail.com>
2024-10-02 23:01:41 -0400

    wip main to renderer sw ipcs

--- shell/browser/api/electron_api_service_worker_main.cc
+++ shell/browser/api/electron_api_service_worker_main.cc
@@ -68,6 +68,63 @@ ServiceWorkerMain::~ServiceWorkerMain() {
   OnDestroyed();
 }
 
+const mojo::Remote<mojom::ElectronRenderer>&
+ServiceWorkerMain::GetRendererApi() {
+  MaybeSetupMojoConnection();
+  return renderer_api_;
+}
+
+void ServiceWorkerMain::MaybeSetupMojoConnection() {
+  if (version_destroyed_) {
+    // RFH may not be set yet if called between when a new RFH is created and
+    // before it's been swapped with an old RFH.
+    LOG(INFO)
+        << "Attempt to setup ServiceWorkerMain connection while render frame "
+           "is disposed";
+    return;
+  }
+
+  if (!renderer_api_) {
+    pending_receiver_ = renderer_api_.BindNewPipeAndPassReceiver();
+    renderer_api_.set_disconnect_handler(
+        base::BindOnce(&ServiceWorkerMain::OnRendererConnectionError,
+                       weak_factory_.GetWeakPtr()));
+  }
+
+  // Wait for RenderFrame to be created in renderer before accessing remote.
+  if (pending_receiver_ &&
+      service_worker_context_->IsLiveRunningServiceWorker(version_id_)) {
+    // TODO: get the interface
+    service_worker_context_->GetRemoteAssociatedInterfaces(version_id_)
+        .GetInterface(&remote_);
+    // render_frame_->GetRemoteInterfaces()->GetInterface(
+    //     std::move(pending_receiver_));
+  }
+}
+
+void ServiceWorkerMain::TeardownMojoConnection() {
+  renderer_api_.reset();
+  pending_receiver_.reset();
+}
+
+void ServiceWorkerMain::OnRendererConnectionError() {
+  TeardownMojoConnection();
+}
+
+void ServiceWorkerMain::Send(v8::Isolate* isolate,
+                             bool internal,
+                             const std::string& channel,
+                             v8::Local<v8::Value> args) {
+  blink::CloneableMessage message;
+  if (!gin::ConvertFromV8(isolate, args, &message)) {
+    isolate->ThrowException(v8::Exception::Error(
+        gin::StringToV8(isolate, "Failed to serialize arguments")));
+    return;
+  }
+
+  GetRendererApi()->Message(internal, channel, std::move(message));
+}
+
 void ServiceWorkerMain::InvalidateState() {
   if (running_info_ != nullptr) {
     running_info_.reset();
